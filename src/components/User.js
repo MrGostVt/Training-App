f550886b9e695da5687d99838f2409bc0d3ad785
Vitalii <vitalijborisenko27@gmail.com>
2026-02-20 16:32:44 +0200

    User icon upload system.

--- src/components/User.js
+++ src/components/User.js
@@ -2,12 +2,58 @@ import React, { useEffect, useState } from "react";
 import '../assets/styles/User.css'
 import { LoadedImages } from "../application/ImageLoad";
 import clientController, { COLORS } from "../application/ClientController";
+import CameraIcon from "../assets/icons/camera.svg";
+import serverController from "../application/ServerController";
+import { UsePreviousState } from "./customHooks/UsePreviousState";
+
 
 export const UserIcon = ({userIconUrl}) => {
+    const [splash, setSplash] = useState(false);
+    const [icon, setIcon] = useState(userIconUrl);
+    const [previousIcon, setPrevious] = UsePreviousState(icon);
+
+    const HandleUpload = async (ev) => {
+        const file = ev.target.files[0];
+        if(!file) { clientController.triggerEvent('show-tip', ['Wrong file', clientController.getColorSetting(2, 'red')]); return;}
+        if( file.size > 3 * 1024 * 1024) { clientController.triggerEvent('show-tip', ['File size must be < 3mb', clientController.getColorSetting(2, 'red')]); return;}
+        
+        const isOk = await serverController.uploadIcon(file, (status) => {
+            clientController.triggerEvent('show-tip', [status == 429? 'Too many attempts. \n Try again later': 'Something went wrong! \n Try again later', clientController.getColorSetting(2, 'red')])
+            setIcon(previousIcon);
+        });
+        
+        if(isOk) {
+            const url = URL.createObjectURL(file);
+            setIcon(url);
+        }
+    }
+    useEffect(() => {
+        if(icon != null && icon.split(serverController.getApi()).length > 1){
+            const img = new Image();
+            img.src = icon;
+            img.onerror = () => {
+                console.log(icon);
+                setIcon(null);
+            }
+        }
+    }, [icon]);
     
     return(
-        <div className="UserIconDefault" 
-        style={{backgroundImage: `url(${userIconUrl? userIconUrl: LoadedImages['UndefinedUser.png']})`}}>
+        <div className="UserIconDefault" onMouseEnter={() => {setSplash(true)}} onMouseLeave={() => {setSplash(false)}} >
+            <div className="IconImage"  style={{backgroundImage: `url(${icon? icon: LoadedImages['UndefinedUser.png']})`}} 
+            onError={() => {console.log("ALEKESY")}}></div>
+            <div className="IconSplash" style={{bottom: splash? '0%': '-30%'}}>
+                <CameraIcon style={{
+                    fill: clientController.getColorSetting(1,COLORS.text),
+                    position: 'relative',
+                }} 
+                    width={25} height={25}/>
+            </div>
+            <input style={{position: 'absolute', width: '100%', height: '100%', 
+                top: 0, opacity: 0}} 
+                type="file" name="avatar" onChange={HandleUpload}
+                accept="image/png, image/jpeg, image/gif"
+            />
         </div>
     );
 }
