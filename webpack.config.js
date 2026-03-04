ef54bbf33b42e2526c3f82acf1ee47b0eea81623
Vitalii <vitalijborisenko27@gmail.com>
2026-03-04 20:55:53 +0200

    config-

--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,10 @@ const dotenv = require('dotenv');
 
 const env = dotenv.config().parsed || {};
 
-// const envKeys = Object.keys(env).reduce((prev, next) => {
-//   prev[`process.env.${next}`] = JSON.stringify(env[next]);
-//   return prev;
-// }, {});
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  return prev;
+}, {});
 new webpack.DefinePlugin({
   'process.env': JSON.stringify(env)
 })
