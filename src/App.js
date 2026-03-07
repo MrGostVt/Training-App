7a641aa7490206cf4648db241c2b1b4d223ee93f
Vitalii <vitalijborisenko27@gmail.com>
2026-03-07 15:39:38 +0200

    loading state

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ const SavedInstance = {
 };
 
 const App = ({}) => {
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
     const [lightLoading, setLightLoading] = useState(false);
     const [pageId, setPage] = useState(0);
     const [modal, setModal] = useState(0);
